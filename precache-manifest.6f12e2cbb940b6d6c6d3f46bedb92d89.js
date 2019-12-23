62d318305ae7606ad2daa67cdfec27e12aa04aa1
Kyle Schiller <kyleschiller@gmail.com>
2019-12-23 10:13:22 -0800

    Updates

---
+++ precache-manifest.6f12e2cbb940b6d6c6d3f46bedb92d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944475b4de75e5f0c9205be601621935",
+    "url": "/mercenary/index.html"
+  },
+  {
+    "revision": "bc0304f0c62f156f83c8",
+    "url": "/mercenary/static/js/2.c8d558d3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/mercenary/static/js/2.c8d558d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ba70147ac73ae9d6405",
+    "url": "/mercenary/static/js/main.74bceea4.chunk.js"
+  },
+  {
+    "revision": "353383438108f40a74ef",
+    "url": "/mercenary/static/js/runtime-main.0b658e64.js"
+  }
+]);
